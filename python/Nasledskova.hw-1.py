317b7985b7f962a0efd263da4e62b723540f993c
purlubru <polli2498@gmail.com>
2016-10-14 18:59:15 +0300

    добавлена новая папка

---
+++ python/Nasledskova.hw-1.py
@@ -0,0 +1,14 @@
+a=input('Введите а ')
+b=input('Введите b ')
+c=input('Введите c ')
+a=int(a)
+b=int(b)
+c=int(c)
+if (a%b==c):
+    print ('a даёт остаток c при делении на b')
+else:
+    print ('a не даёт остаток c при делении на b')
+if (a/b==c):
+    print ('a разделить на b будет с')
+else:
+    print ('а разделить на b будет не с')
